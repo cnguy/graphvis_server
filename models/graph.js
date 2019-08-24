e835455343a2d43e2566535795e32e6af6ae9408
brotaku <brian.caulfield13@gmail.com>
2019-08-24 15:57:30 -0700

    Finalized model schema for the database

--- models/graph.js
+++ models/graph.js
@@ -2,8 +2,46 @@ const mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 
-var GraphSchema = new Schema({
-    name: String
+// An edge is contained in an edge list, so "to" represents the destination of the edge, 
+// where the origin is the node object this edge is contained in
+var EdgeSchema = new Schema({
+    to: Number,             //destination
+    weight: Number          // float: weight of edge
 })
 
-module.exports = mongoose.model('Graph', GraphSchema)
+var OrbitSchema = new Schema({
+    id: Number,             // orbit id: [1, 73]
+    frequency: Number       // frequency this node appears in this orbit
+})
+
+var NodeSchema = new Schema({
+    id: Number,             //id of the node
+    name: String,           //name of the node
+    edges: [EdgeSchema],    //edge list
+    orbits: [OrbitSchema]   //orbit list
+})
+
+// Note: The top 5 fields are filenames, not actual data. 
+// Keep record of the filename in case the user wants to reference which files were used for this graph pair
+var GraphSchema = new Schema({
+    edge_list: String, 
+    weight_matrix: String, 
+    coordinates: String, 
+    node_names: String,
+    node_ids: String, 
+    orbits: String, 
+    nodes: [NodeSchema]
+})
+
+var GraphPairSchema = new Schema({
+    hash: String,                               //the hash to access this graph again
+    name: String,                               //name of the graph
+    author: {type: String, default: ''},        //graph creator
+    created: {type: Date, default: Date.now},   // date graph was created
+    graphs: {
+        ocd: GraphSchema,
+        con: GraphSchema
+    }
+})
+
+module.exports = mongoose.model('Graph', GraphPairSchema)
